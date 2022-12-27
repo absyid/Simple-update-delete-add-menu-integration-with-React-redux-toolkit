ab381a19d0f0a0bfd6abef61757c00e702c2c7c3
refactory-engineering <engineering@refactory.id>
2022-12-27 11:28:08 +0700

    React redux sederhana

---
+++ src/components/ShowProduct.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getProducts,
+  productSelectors,
+  deleteProduct,
+} from "../features/productSlice";
+import { Link } from "react-router-dom";
+
+const ShowProduct = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(productSelectors.selectAll);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <p class="panel-heading mt-5 is-size-4 is-family-secondary has-background-info has-text-white-bis">
+        Update Resto Pak Haji
+      </p>
+      <div className="box mt-2">
+        <table className="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Menu</th>
+              <th>Price (Rp)</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <tr key={product.id}>
+                <td>{index + 1}</td>
+                <td>{product.title}</td>
+                <td>{product.price}</td>
+                <td>
+                  <Link
+                    to={"edit/${product.id}"}
+                    className="button is-info is-small"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => dispatch(deleteProduct(product.id))}
+                    className="button is-danger is-small"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Link
+          to="add"
+          className="button is-succes mb-3 has-background-info has-text-white-bis"
+        >
+          Add New
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ShowProduct;
